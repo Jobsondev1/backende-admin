bcfdde483c3e15912a9a5defdaceef40e3a9adf8
jobsondev1 <jammeloalves@gmail.com>
2021-06-29 23:10:19 -0300

    validacao se existe usuario com email

--- src/controllers/usuariosControllers.js
+++ src/controllers/usuariosControllers.js
@@ -2,11 +2,22 @@ const Usuario = require('../models/usuarioModel');
 //const bccrypt = require('bcrypt');
 
 
-
 module.exports = {
     
     async store(req, res) {
-
+    /**
+     * consultar se existe usuario por email
+     */
+       const userExiste = await Usuario.findOne({email_usuario: req.body.email_usuario});
+        if(userExiste) {
+        return res.status(400).json({
+            error: true,
+            messge: "Éste usuario já existe!"
+        })
+    }
+        /**
+     * Pega dados do corpo do fronte
+     */
         const {nome_usuario, email_usuario, senha_usuario, tipo_usuario} = req.body;
 
         let data = {nome_usuario,
@@ -15,6 +26,9 @@ module.exports = {
              tipo_usuario
             };
 
+    /**
+     * Criar um usuario no banco
+     */
         await Usuario.create(data, (err) =>{
            if(err) return res.status(400).json({
                error:true,
@@ -27,37 +41,5 @@ module.exports = {
         })
     } 
 
-
- /*   index(req,res){
-        res.json({message: 'Hello world from controller Usuario'});
-    },
-    
-
-  async create(req,res){
-     const {nome_usuario, email_usuario, senha_usuario, tipo_usuario} = req.body;
-
-     let data = {};
-
-     let user = Usuario.findOne({email_usuario});
-     if(!user){
-         data = {nome_usuario, email_usuario, senha_usuario, tipo_usuario};
-         user = await Usuario.create(data);
-         return res.status(200).json(user);
-     }else{
-         return res.status(500).json("erro no sevidor");
-     }*/
-
-    /* try{
-        const userExiste = await Usuario.findOne({email_usuario})
-        if(!userExiste) return res.status(400).send({message: "não existe"})
-
-        const validaSenha = await bccrypt.compare(senha_usuario, userExiste.senha_usuario)
-        if(!validaSenha) return res.status(400).send({message: "Senha invalido"})
-
-        return res.status(200).send(userExiste)
-     }catch(err){
-         return res.status(400).send(err)
-     }*/
-   
     
 }
